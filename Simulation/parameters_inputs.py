aa3ecdb96820b29678e238fb45c84e1dfcda5236
carinebeatrici <carine_beatrici@yahoo.com.br>
2023-07-17 14:26:44 -0300

    [UPD] Initial parameters updated to be the same as the paper

--- Simulation/parameters_inputs.py
+++ Simulation/parameters_inputs.py
@@ -1,22 +1,22 @@
-parameters = {
-"size_x": 100,
-"size_y": 100,
-"size_z": 100,
-"MC_steps": 1000,
-"temperature": 10.0,
-"drop_size": 30,
-"energy_gas_gas":0.0,
-"energy_gas_liq":10.0,
-"energy_gas_sol":10.0,
-"energy_liq_liq":10.0,
-"energy_liq_sol":20.0,
-"energy_sol_sol":100.0,
-"obstacle_hight": 5,
+parameters = { 
+"size_x": 240, 
+"size_y": 240, 
+"size_z": 240, 
+"MC_steps": 1000000, 
+"temperature": 13.0, 
+"drop_size": 50, 
+"energy_gas_gas": 0.0, 
+"energy_gas_liq": 2.7, 
+"energy_gas_sol": 0.96, 
+"energy_liq_liq": 0.0, 
+"energy_liq_sol": 2.31, 
+"energy_sol_sol": 0.0, 
+"obstacle_hight": 5, 
 "obstacle_width": 5, 
-"obstacle_distance": 5,
-"gravity": 50,
-"motility": 100,
-"persistance": 0.9
+"obstacle_distance": 5, 
+"gravity": 0, 
+"motility": 0.0, 
+"persistance": 0.0 
 }
 
 toto = 100
